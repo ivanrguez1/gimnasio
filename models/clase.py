3c08aa9a52902bc5ad47ed47faf8da4cffb73096
ivanrguez1 <ivanrguez1@yahoo.es>
2020-01-20 16:10:17 +0100

    001 -> Primer commit a remoto

---
+++ models/clase.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from cups import require
+
+class clase(models.Model):
+    _name = 'gimnasio.clase'
+    
+    nombre = fields.Char('Clase', size=64, required=True)
+    inicio = fields.Datetime('Inicio', required=True, autodate=True)
+    fin = fields.Datetime('Fin', required=True, autodate=True)
+# class gimnasio(models.Model):
+#     _name = 'gimnasio.gimnasio'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
